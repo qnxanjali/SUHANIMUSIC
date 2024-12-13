59f902bd1f21f52dcf6984ec1c1e3079ce1ee623
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-12-14 01:16:49 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

--- SUHANIMUSIC/plugins/bot/mustjoin.py
+++ SUHANIMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from SUHANIMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "+vX5qi7eJ5MlhYTM1"
+MUST_JOIN = "venompratapchat"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
